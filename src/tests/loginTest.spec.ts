f04f49c73e2c0ce997fcebf70a9cbe25a4341857
mukul jha <mukuljha302@gmail.com>
2024-03-17 18:57:01 +0530

    Fifth commit

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -4,25 +4,25 @@ import HomePage from "../pages/HomePage";
 import { decrypt, encrypt } from "../utils/CryptojsUtil";
 
 
-// test("test",async ({page})=>{
+test("test",async ({page})=>{
 
 
-// const loginPage=new LoginPage(page);
+const loginPage=new LoginPage(page);
 
-// await loginPage.navigateToLoginPage();
-// await loginPage.fillUsername("mukuljha302-jcw1@force.com");
-// await loginPage.fillPassword("Mukul392@");
-
-
-// const homePage= await loginPage.clickLoginButton();
-// //await page.waitForTimeout(15000);
-
-// await homePage.expectServiceTitleToBeVisible();
-
-// })
+await loginPage.navigateToLoginPage();
+await loginPage.fillUsername("mukuljha302-jcw1@force.com");
+await loginPage.fillPassword("Mukul392@");
 
 
-test.skip("Sample env test", ({page})=>{
+const homePage= await loginPage.clickLoginButton();
+//await page.waitForTimeout(15000);
+
+await homePage.expectServiceTitleToBeVisible();
+
+})
+
+
+test("Sample env test", ({page})=>{
     console.log(process.env.NODE_ENV);
     console.log(process.env.password);
     console.log(process.env.username);
@@ -32,15 +32,13 @@ test.skip("Sample env test", ({page})=>{
     })
     
 
-//     test("Sample env Test",async({page})=> {
-// const plainText="Hello, Mars!";
-// const encryptedText=encrypt(plainText);
-// console.log("SALT",process.env.SALT);
-// console.log("Encrypted",encryptedText);
-// const decryptedText= decrypt(encryptedText);
-// console.log("Decrypted",decryptedText);
+    test("Encryption test",async({page})=> {
+const plainText="Hello, Mars!";
+const encryptedText=encrypt(plainText);
+console.log("SALT",process.env.SALT);
+console.log("Encrypted",encryptedText);
+const decryptedText= decrypt(encryptedText);
+console.log("Decrypted",decryptedText);
 
-
-
-//     });
+    });
     
